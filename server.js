3216e02db60d8c1c8a4606bc3195116c5b44905f
newcomer0560 <ranaashutosh0560@gmail.com>
2017-04-18 15:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/image.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'image.png'));
+app.get('/ui/img1.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'img1.png'));
 });
 
 
